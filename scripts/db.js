3e2466ba5cb7bfe5d7aa7e5f57da72d67e8e790e
Marija Galiot <marija.g1990@gmail.com>
2026-04-27 17:10:12 +0200

    add main

---
+++ scripts/db.js
@@ -0,0 +1,29 @@
+let dishes = [
+    {
+        "name": "Veggie mushroom black burger",
+        "price": 16.90,
+        "ingredients": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+        "amount": 0
+    },
+
+    {
+        "name": "All meat burger",
+        "price": 15.90,
+        "ingredients": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
+        "amount": 0
+    },
+
+    {
+        "name": "Beef red burger",
+        "price": 14.90,
+        "ingredients": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+        "amount": 0
+    },
+
+    {
+        "name": "Big chicken burger",
+        "price": 15.90,
+        "ingredients": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+        "amount": 0
+    }
+]
