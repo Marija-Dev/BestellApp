ae058c13eda712a7c8626bb66be0eae8d0267330
Marija Galiot <marija.g1990@gmail.com>
2026-05-01 12:50:44 +0200

    add db

--- scripts/db.js
+++ scripts/db.js
@@ -1,4 +1,4 @@
-let dishes = [
+let burgerSandwiches = [
     {
         "name": "Veggie mushroom black burger",
         "price": 16.90,
@@ -26,4 +26,64 @@ let dishes = [
         "ingredients": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
         "amount": 0
     }
+];
+
+let pizza = [
+    {
+        "name": "Pizza Margherita",
+        "price": 11.90,
+        "ingredients": "Tomato Sauce, Mozzarella",
+        "amount": 0
+    },
+
+    {
+        "name": "Pizza Chorizo",
+        "price": 13.90,
+        "ingredients": "Tomato slices, Mozzarella, Chorizo",
+        "amount": 0
+    },
+
+    {
+        "name": "Pizza Funghi",
+        "price": 12.90,
+        "ingredients": "Red onion, Olives, Mushrooms, Mozzarella",
+        "amount": 0
+    },
+
+    {
+        "name": "Quattro Formaggi with Chicken",
+        "price": 15.90,
+        "ingredients": "Chicken, Mozzarella, gorgonzola, Fontina, Parmigiano Reggiano",
+        "amount": 0
+    },
+];
+
+let salad = [
+    {
+        "name": "Warm beef arugula salad",
+        "price": 16.90,
+        "ingredients": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+        "amount": 0
+    },
+
+      {
+        "name": "Mini green Salad",
+        "price": 7.90,
+        "ingredients": "Green salad, Cucumber, Carrots, Parsley, Radishes",
+        "amount": 0
+    },
+
+      {
+        "name": "Green Salad with sea food",
+        "price": 16.90,
+        "ingredients": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+        "amount": 0
+    },
+
+      {
+        "name": "Vegan green salad with tofu",
+        "price": 14.90,
+        "ingredients": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+        "amount": 0
+    },
 ]
